6cbc35ba9a3847f803a2162bc4ac07b2c25925de
Anmol1696 <anmol1696@gmail.com>
2025-09-03 21:51:38 +0400

    initial testing setup for extended definations

---
+++ packages/schema-sdk/__tests__/openapi.extended.generate.test.ts
@@ -0,0 +1,91 @@
+import { writeFileSync } from 'fs';
+
+import schema from '../../../__fixtures__/openapi/swagger_extended.json';
+import { generateOpenApiClient } from '../src/openapi';
+import { getDefaultSchemaSDKOptions } from '../src/types';
+
+it('swagger', () => {
+  const options = getDefaultSchemaSDKOptions({
+    clientName: 'KubernetesClient',
+    includeSwaggerUrl: true,
+    exclude: [
+      '*.v1beta1.*',
+      '*.v2beta1.*',
+      'io.k8s.api.events.v1.EventSeries',
+      'io.k8s.api.events.v1.Event',
+      'io.k8s.api.flowcontrol*',
+    ],
+  });
+  const code = generateOpenApiClient(
+    {
+      ...options,
+      paths: {
+        exclude: ['*flowschema*', '*v1beta1*', '*v2beta1*'],
+        excludeRequests: ['head', 'options'],
+        excludeTags: [
+          'storage_v1beta1',
+          '*v1beta1',
+          '*v2beta1',
+          '*v1beta1*',
+          '*v2beta1*',
+        ],
+      },
+      includeTypeComments: true,
+      includeMethodComments: true,
+      mergedParams: false,
+      namingStrategy: {
+        useLastSegment: true,
+        renameMap: {
+          'io.k8s.api.discovery.v1.EndpointPort': 'DiscoveryEndpointPort',
+          'io.k8s.apiextensions-apiserver.pkg.apis.apiextensions.v1.ServiceReference':
+            'ApiExtServiceReference',
+          'io.k8s.apiextensions-apiserver.pkg.apis.apiextensions.v1.WebhookClientConfig':
+            'ApiExtWebhookClientConfig',
+          'io.k8s.api.admissionregistration.v1.ServiceReference':
+            'AdmissionServiceReference',
+        },
+      },
+    },
+    schema as any
+  );
+  expect(code).toMatchSnapshot();
+  writeFileSync(
+    __dirname + '/../../../__fixtures__/output/swagger-extended-client.ts',
+    code
+  );
+});
+
+it('merged', () => {
+  const options = getDefaultSchemaSDKOptions({
+    // include: [
+    //     '*.v1.*'
+    // ],
+    clientName: 'KubernetesClient',
+    includeSwaggerUrl: true,
+    includeMethodComments: true,
+    namingStrategy: {
+      useLastSegment: true,
+    },
+  });
+  const code = generateOpenApiClient(
+    {
+      ...options,
+      version: 'v1',
+      mergedParams: true,
+    },
+    schema as any
+  );
+  expect(code).toMatchSnapshot();
+  writeFileSync(
+    __dirname + '/../../../__fixtures__/output/swagger-extended-client.merged.ts',
+    code
+  );
+});
+
+it('openapi', () => {
+  const data = Object.keys(schema.definitions);
+  writeFileSync(
+    __dirname + '/../../../__fixtures__/output/swagger-extended-definitions.json',
+    JSON.stringify(data, null, 2)
+  );
+});
